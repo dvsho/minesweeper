3fcebf1ba87287e095cecefc42c7b8c604ad0eab
David Ho <dho2004@icloud.com>
2024-09-23 14:14:24 -0700

    Added drawable assets

--- app/src/main/java/com/example/cs310_minesweeper/ResultActivity.java
+++ app/src/main/java/com/example/cs310_minesweeper/ResultActivity.java
@@ -24,7 +24,7 @@ public class ResultActivity extends AppCompatActivity {
         timeView.setText("Time: " + seconds + " seconds");
 
         restartButton.setOnClickListener(v -> {
-            Intent intent = new Intent(ResultActivity.this, com.example.minesweeper.MainActivity.class);
+            Intent intent = new Intent(ResultActivity.this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
